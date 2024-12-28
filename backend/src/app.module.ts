ff7c8750ce9ef01ad4749b949db7edb90ca87e7b
Cadart <cadart.val@gmail.com>
2024-12-28 23:17:02 +0100

    initialize docker-compose local for backend and db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './config/datasource';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        await AppDataSource.initialize();
+        return {
+          ...AppDataSource.options,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
